decde6e7326af530c14abd23f42b6a9a5417f383
Haya-chb <haya.chaibi@hotmail.com>
2026-03-16 06:03:31 +0100

    Dowloaded and created root

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,7 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  index("routes/home.jsx"),
+  route("game", "routes/game.jsx"),
+  route("score", "routes/score.jsx"),
+] satisfies RouteConfig;
